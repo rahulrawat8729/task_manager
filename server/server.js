4ac26c41b6da4ef0f0fcf2e398d264330a24c3d1
Rahul Singh Rawat <rahulrawat8729@gmail.com>
2025-12-06 18:57:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,8 @@ const app = express();
 // app.use(express.json());
 const allowedOrigins = [
     'http://localhost:5000', // local dev
-    'https://task-manager-new-ochre.vercel.app/' // deployed frontend
+    'https://task-manager-2ah7.onrender.com' // deployed frontend
+    
 ];
 
 app.use(cors({
